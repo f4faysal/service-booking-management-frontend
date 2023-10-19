51102ad8e4826462cd98083f70ce147b6c62e253
f4faysal <f4faysals@gmail.com>
2023-10-19 20:30:08 +0600

    Carasol done

--- src/components/ui/hero-section.tsx
+++ src/components/ui/hero-section.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { Carousel } from "antd";
+import { Button, Carousel } from "antd";
+import Image from "next/image";
+import Link from "next/link";
 
 const contentStyle: React.CSSProperties = {
   height: "400px",
@@ -14,16 +16,204 @@ const HeroSection = () => {
   return (
     <Carousel autoplay>
       <div>
-        <h3 style={contentStyle}>1</h3>
+        <div
+          style={{
+            position: "relative",
+          }}
+        >
+          <Image
+            src="https://res.cloudinary.com/dhvuyehnq/image/upload/v1697724407/zoxgbpdqkoipuihf01x4.webp"
+            alt="hero"
+            width={1920}
+            height={600}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: "30%",
+              left: "70%",
+            }}
+          >
+            <h1
+              style={{
+                transform: "translate(-50%, -50%)",
+                fontSize: "35px",
+                color: "black",
+                fontWeight: "bold",
+                fontFamily: "sans-serif",
+              }}
+            >
+              The Best Online Home Cleaning Service
+            </h1>
+            <p
+              style={{
+                marginTop: "30px",
+                transform: "translate(-50%, -50%)",
+                fontSize: "17px",
+                color: "black",
+                fontFamily: "sans-serif",
+              }}
+            >
+              Discover the best online home cleaning service for a spotless and
+              stress-free living space. Our experienced professionals provide
+              top-notch cleaning, using eco-friendly products. Enjoy a clean,
+              healthy home without the hassle. Book now for a sparkling,
+              sanitized living environment.
+            </p>
+            <div
+              style={{
+                marginTop: "30px",
+                position: "absolute",
+                top: "80%",
+                left: "-50%",
+              }}
+            >
+              <Link href="/services">
+                <Button
+                  style={{
+                    width: "150px",
+                    height: "50px",
+                  }}
+                >
+                  Book Now
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
       <div>
-        <h3 style={contentStyle}>2</h3>
+        <div
+          style={{
+            position: "relative",
+          }}
+        >
+          <Image
+            src="https://res.cloudinary.com/dhvuyehnq/image/upload/v1697724438/nabhgxbxmjdwzugpdsuu.webp"
+            alt="hero"
+            width={1920}
+            height={600}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: "30%",
+              left: "70%",
+            }}
+          >
+            <h1
+              style={{
+                transform: "translate(-50%, -50%)",
+                fontSize: "35px",
+                color: "white",
+                fontWeight: "bold",
+                fontFamily: "sans-serif",
+              }}
+            >
+              Online Car Service Appointment Booking
+            </h1>
+            <p
+              style={{
+                marginTop: "30px",
+                transform: "translate(-50%, -50%)",
+                fontSize: "17px",
+                color: "white",
+                fontFamily: "sans-serif",
+              }}
+            >
+              Our online car service appointment booking platform streamlines
+              vehicle maintenance. Schedule appointments, choose services, and
+              find certified mechanics, ensuring a convenient and efficient
+              automotive experience.
+            </p>
+            <div
+              style={{
+                marginTop: "30px",
+                position: "absolute",
+                top: "80%",
+                left: "-50%",
+              }}
+            >
+              <Link href="/services">
+                <Button
+                  style={{
+                    width: "150px",
+                    height: "50px",
+                  }}
+                >
+                  Book Now
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
       <div>
-        <h3 style={contentStyle}>3</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>4</h3>
+        <div
+          style={{
+            position: "relative",
+          }}
+        >
+          <Image
+            src="https://res.cloudinary.com/dhvuyehnq/image/upload/v1697725694/y3cigdouvn4corhzbaz7.webp"
+            alt="hero"
+            width={1920}
+            height={600}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: "30%",
+              left: "70%",
+            }}
+          >
+            <h1
+              style={{
+                transform: "translate(-50%, -50%)",
+                fontSize: "35px",
+                color: "white",
+                fontWeight: "bold",
+                fontFamily: "sans-serif",
+              }}
+            >
+              Online Spa Appointment Service Booking
+            </h1>
+            <p
+              style={{
+                marginTop: "30px",
+                transform: "translate(-50%, -50%)",
+                fontSize: "17px",
+                color: "white",
+                fontFamily: "sans-serif",
+              }}
+            >
+              Experience relaxation and rejuvenation at your fingertips! Our
+              online spa appointment service allows you to book massages,
+              facials, and more with ease. Discover tranquility with a few
+              clicks, choosing your preferred treatments and appointment times.
+              Indulge in self-care effortlessly.
+            </p>
+            <div
+              style={{
+                marginTop: "30px",
+                position: "absolute",
+                top: "80%",
+                left: "-50%",
+              }}
+            >
+              <Link href="/services">
+                <Button
+                  style={{
+                    width: "150px",
+                    height: "50px",
+                  }}
+                >
+                  Book Now
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </Carousel>
   );
